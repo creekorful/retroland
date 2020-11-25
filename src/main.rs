b85c81aa345401375fb0ba0c1ef959c9593c5b54
Aloïs Micard <lunamicard@gmail.com>
2020-11-25 17:00:38 +0100

    Sfml hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use sfml::graphics::{Color, RenderTarget, RenderWindow};
+use sfml::window::{Event, Style};
+
 fn main() {
-    println!("Hello, world!");
+    let mut window =
+        RenderWindow::new((800, 600), "Retroland", Style::DEFAULT, &Default::default());
+
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            match event {
+                Event::Closed => window.close(),
+                _ => {}
+            }
+        }
+
+        window.clear(Color::BLUE);
+        window.display();
+    }
 }
