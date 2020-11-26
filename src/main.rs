c27d87c3ad889f8f90c70ac284d7b3bded965ba6
Aloïs Micard <lunamicard@gmail.com>
2020-11-27 00:15:45 +0100

    More work on renderer

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     window.set_vertical_sync_enabled(true);
 
     let tile_map = TileMap::new((10, 10), 1);
-    let renderer = TileMapRenderer::new(&tile_map);
+    let renderer = TileMapRenderer::new(&tile_map, window.size(), (5, 5).into());
 
     while window.is_open() {
         while let Some(event) = window.poll_event() {
--- src/tilemap/renderer.rs
+++ src/tilemap/renderer.rs
@@ -1,7 +1,8 @@
 use crate::tilemap::TileMap;
 use sfml::graphics::{
-    Color, Drawable, RectangleShape, RenderStates, RenderTarget, Shape, Transformable,
+    Color, Drawable, RectangleShape, RenderStates, RenderTarget, Shape, Transformable, View,
 };
+use sfml::system::Vector2u;
 
 /// Tile map renderer is used to render a tile map on the screen
 pub struct TileMapRenderer<'s> {
@@ -9,16 +10,27 @@ pub struct TileMapRenderer<'s> {
 }
 
 impl<'s> TileMapRenderer<'s> {
-    pub fn new(tile_map: &TileMap) -> Self {
-        let size = tile_map.size();
+    pub fn new<T: Into<Vector2u>>(tile_map: &TileMap, screen_size: T, viewport_size: T) -> Self {
+        let tile_map_size = tile_map.size();
+        let screen_size = screen_size.into();
+
         let mut tiles = Vec::new();
 
+        // Determinate tile size to fix them on whole screen
+        let tile_width = screen_size.x / tile_map_size.x;
+        let tile_height = screen_size.y / tile_map_size.y;
+        let tile_size = if tile_width > tile_height {
+            tile_width
+        } else {
+            tile_height
+        } as f32;
+
         // At the moment only draw layer 0
-        for y in 0..size.y {
-            for x in 0..size.x {
+        for y in 0..tile_map_size.y {
+            for x in 0..tile_map_size.x {
                 let mut tile = RectangleShape::new();
-                tile.set_size((70.0, 70.0));
-                tile.set_position((x as f32 * 70.0, y as f32 * 70.0));
+                tile.set_size((tile_size, tile_size));
+                tile.set_position((x as f32 * tile_size, y as f32 * tile_size));
                 tile.set_fill_color(Color::RED);
                 tile.set_outline_color(Color::BLACK);
                 tile.set_outline_thickness(1.0);
@@ -45,12 +57,17 @@ impl<'s> Drawable for TileMapRenderer<'s> {
 #[cfg(test)]
 mod tests {
     use crate::tilemap::{TileMap, TileMapRenderer};
+    use sfml::system::Vector2f;
 
     #[test]
     fn test_tile_map_renderer_new() {
         let tile_map = TileMap::new((5, 5), 1);
-        let renderer = TileMapRenderer::new(&tile_map);
+        let renderer = TileMapRenderer::new(&tile_map, (1920, 1080), (5, 5));
 
         assert_eq!(renderer.tiles.len(), 25);
+        assert_eq!(
+            renderer.tiles.get(0).unwrap().size(),
+            Vector2f::new(384.0, 384.0)
+        );
     }
 }
