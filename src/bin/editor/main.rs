a085fc27e22243ae4b95569169789f8d54231c98
Aloïs Micard <lunamicard@gmail.com>
2020-11-30 08:06:08 +0100

    Moar blocks

--- src/bin/editor/main.rs
+++ src/bin/editor/main.rs
@@ -20,12 +20,12 @@ fn load_textures<P: AsRef<Path>>(
     let mut textures = BTreeMap::new();
 
     let mut idx = 1;
-    for i in 0..5 {
+    for x in 0..5 {
         textures.insert(
             idx,
             Texture::from_file_with_rect(
                 &format!("{}/grass.png", assets_dir.as_ref().display()),
-                &IntRect::new(i * 16, 0, 16, 16),
+                &IntRect::new(x * 16, 0, 16, 16),
             )
             .ok_or_else(|| "unable to load texture".to_string())?,
         );
@@ -42,10 +42,32 @@ fn load_textures<P: AsRef<Path>>(
                     &format!("{}/houses.png", assets_dir.as_ref().display()),
                     &IntRect::new(x * 16, y * 16, 16, 16),
                 )
-                    .ok_or_else(|| "unable to load texture".to_string())?,
+                .ok_or_else(|| "unable to load texture".to_string())?,
             );
             idx += 1;
         }
+    }
+    for x in 0..3 {
+        textures.insert(
+            idx,
+            Texture::from_file_with_rect(
+                &format!("{}/markets.png", assets_dir.as_ref().display()),
+                &IntRect::new(x * 16, 2 * 16, 16, 16),
+            )
+            .ok_or_else(|| "unable to load texture".to_string())?,
+        );
+        idx += 1;
+    }
+    for y in 0..4 {
+        textures.insert(
+            idx,
+            Texture::from_file_with_rect(
+                &format!("{}/resources.png", assets_dir.as_ref().display()),
+                &IntRect::new(0, y * 16, 16, 16),
+            )
+            .ok_or_else(|| "unable to load texture".to_string())?,
+        );
+        idx += 1;
     }
     for x in 0..2 {
         for y in 0..2 {
@@ -59,6 +81,28 @@ fn load_textures<P: AsRef<Path>>(
             );
             idx += 1;
         }
+    }
+    for x in 0..4 {
+        textures.insert(
+            idx,
+            Texture::from_file_with_rect(
+                &format!("{}/wheatfields.png", assets_dir.as_ref().display()),
+                &IntRect::new(x * 16, 0, 16, 16),
+            )
+            .ok_or_else(|| "unable to load texture".to_string())?,
+        );
+        idx += 1;
+    }
+    for x in 1..4 {
+        textures.insert(
+            idx,
+            Texture::from_file_with_rect(
+                &format!("{}/trees.png", assets_dir.as_ref().display()),
+                &IntRect::new(x * 16, 0, 16, 16),
+            )
+            .ok_or_else(|| "unable to load texture".to_string())?,
+        );
+        idx += 1;
     }
 
     Ok(textures)
